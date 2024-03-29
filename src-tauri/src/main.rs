e0256d3930d309c8440c98955d95ff6c8e7e2b0f
Vahor <me@vahor.fr>
2024-03-29 13:04:28 +0100

    add App state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+pub mod app;
+use app::App;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    App::instance().run(name)
 }
 
 fn main() {
-    tauri::Builder::default()
+    App::init();
+
+    let app = tauri::Builder::default();
+
+    let app = app
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_fs::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_fs::init());
+
+    app.invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
